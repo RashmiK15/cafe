6ef9251a5712b5a4c2f971cba0565c36c53f2f84
RashmiK15 <66722722+RashmiK15@users.noreply.github.com>
2022-02-18 12:10:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import DrinkList from './components/DrinkList';
+import Footer from './components/Footer';
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      allDrinks : [
+        {
+          id: 1,
+          name: 'User1',
+          size: 'Medium',
+          milk:'Regular',
+          isHot: 'Hot',
+          drink:'Irish Cappuccino',
+          flavour:'caramel'
+
+        },
+    
+      ]
+    }
+    this.addNewDrink = this.addNewDrink.bind(this);
+  }
+
+  addNewDrink(drink){
+    let allDrinksAdded = this.state.allDrinks
+    allDrinksAdded.push({
+        id:       allDrinksAdded.length+1, 
+        name:     drink.newName, 
+        size:     drink.newDrinkSize,
+        milk:     drink.newDrinkMilk,
+        isHot:    drink.newDrinkHot,
+        drink:    drink.newDrink,
+        flavour:  drink.newDrinkFlavour
+    })
+    this.setState({
+      allDrinks: allDrinksAdded
+    });
+  }
+
+  render(){
+    return(
+      <>
+      <Header count={this.state.allDrinks} />
+      <DrinkList list={this.state.allDrinks} toAddNewDrink= {(data) => this.addNewDrink(data)} />
+      <Footer />
+
+      </>
+    )
+
+  }
+}
+
+export default App;
